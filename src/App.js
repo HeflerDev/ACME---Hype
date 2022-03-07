e64ea8f05993c1bf8962cca72c8633a17d06e4ab
HeflerDev <heflerdev@gmail.com>
2022-03-07 21:59:04 +0000

    Project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+} from "react-router-dom";
+
+const App = () => {
+   return (
+      <Router>
+         <div>Setup</div>;
+      </Router>
+   )
+};
+
+export default App;
