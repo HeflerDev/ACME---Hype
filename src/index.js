dc90bb61f98a7fa13e4704afe7ce326a6068c72c
HeflerDev <heflerdev@gmail.com>
2022-03-08 14:48:58 +0000

    Aditional Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App.js";
-import "./styles/master.scss";
+import App from './views/App.js'
+import './styles/master.scss'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
