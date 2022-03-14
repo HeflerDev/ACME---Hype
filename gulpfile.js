220c7c6ad20347da5d678eec4ed11fc482a2abff
HeflerDev <heflerdev@gmail.com>
2022-03-14 06:38:40 +0000

    Adjust gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,42 @@
-var gulp = require("gulp");
-var nodemon = require("gulp-nodemon");
-var browserSync = require("browser-sync").create();
+const gulp = require('gulp')
+const nodemon = require('gulp-nodemon')
+const browserSync = require('browser-sync').create()
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config.js')
 
-gulp.task("gulp_nodemon", function () {
+gulp.task('gulp_nodemon', function () {
   nodemon({
-    script: "server.js",
-    ext: "js html css",
-    env: { NODE_ENV: "development" },
-  });
-});
+    script: 'server.js',
+    ext: 'js html css',
+    env: { NODE_ENV: 'development' }
+  })
+})
 
-gulp.task("sync", function () {
+gulp.task('assets', function () {
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stats) => {
+      if (err) {
+        return reject(err)
+      }
+      if (stats.hasErrors()) {
+        return reject(new Error(stats.compilation.errors.join('\n')))
+      }
+      resolve()
+    })
+  })
+})
+
+gulp.task('sync', function () {
   browserSync.init({
     port: 3002,
-    proxy: "http://localhost:3000/",
+    proxy: 'http://localhost:3000/',
     ui: { port: 3003 },
     reloadDelay: 1000
-  });
+  })
   gulp
-    .watch(["./**/*.js", "./**/*.html", "./**/*.css"])
-    .on("change", browserSync.reload);
-});
+    .watch(['./**/*.js', './**/*.html', './**/*.css'])
+    .on('change', browserSync.reload)
+})
 
-exports.build = gulp.parallel(["gulp_nodemon", "sync"]);
+exports.dev = gulp.parallel(['gulp_nodemon', 'sync'])
+exports.build = gulp.series(['gulp_nodemon', 'assets'])
